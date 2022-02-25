d1ba4b914c34bd5af496b4810af6d852db7a7e2b
NFS86 <jarbull86@gmail.com>
2022-02-25 11:59:27 +0700

    Test full
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- build.sh
+++ build.sh
@@ -9,10 +9,12 @@ lunch aosp_rosy-userdebug
 export TZ=Asia/Jakarta
 export BUILD_USERNAME=rosy
 export BUILD_HOSTNAME=userdebug
+export TARGET_BUILD_PIXELBLASTER=true
+export BLASTER_BUILD_VARIANT=GAPPS
+export TARGET_FACE_UNLOCK_SUPPORTED=true
 curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
 mka bacon -j10 &
 sleep 100m
-kill %1
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P

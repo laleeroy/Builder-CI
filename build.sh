e0d4bb82f1b06533cbc7aa8a515fc665c542f539
Asif <najmulhasan3609@gmail.com>
2022-03-01 17:47:12 +0600

    initial build

--- build.sh
+++ build.sh
@@ -1,16 +1,18 @@
-#sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b arrow-12 .repo/local_manifests
+# sync rom
+repo init --depth=1 --no-repo-verify -u git://github.com/Octavi-OS/platform_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b oct-11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch arrow_rosy-userdebug
+lunch octavi_rosy-userdebug
 export TZ=Asia/Dhaka
 export BUILD_USERNAME=rosy
 export BUILD_HOSTNAME=userdebug
 curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
-m bacon
+brunch rosy &
+sleep 95m
+kill %1
 
 
 # upload rom

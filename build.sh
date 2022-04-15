fea076ffd00d4431e01ee78706f1f116a55fece7
laleeroy <gabwielcastwo@gmail.com>
2022-04-15 15:28:34 +0800

    Test
    Signed-off-by: laleeroy <gabwielcastwo@gmail.com>

--- build.sh
+++ build.sh
@@ -1,17 +1,14 @@
-#sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-projects/local_manifest --depth 1 -b PE-11 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j10
+# sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/laleeroy/local_manifest.git --depth 1 -b alioth-arc .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_rosy-userdebug
-export TZ=Asia/Jakarta
-export BUILD_USERNAME=rosy
-export BUILD_HOSTNAME=userdebug
+lunch aosp_alioth-userdebug
+export TZ=Asia/Manila
 curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
-mka bacon -j10
-
+mka sepolicy
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
